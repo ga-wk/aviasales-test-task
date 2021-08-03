7440995e41d1a5802e4228b1eb283d23d74f3d82
Andrey <agazzaev88@gmail.com>
2021-08-03 16:26:22 +0700

    Receiving data from the server

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,32 +1,83 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { mainConst } from '../../constants/main';
 import logo from '../../svg/logo.svg';
 import Filter from '../filter';
 import Tabs from '../tabs';
 import './index.css';
 
+async function fetchDataGet(url, success, fail) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    success(json);
+  } catch (error) {
+    fail(error);
+  }
+}
+
 function Main() {
   const [stops, setStops] = useState([2, 3, 4]);
-  const [tab, setTab] = useState(0)
+  const [tab, setTab] = useState(0);
+
+  const [searchId, setSearchId] = useState(undefined);
+  const [tickets, setTickets] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isTry, setIsTry] = useState(0);
+  const [isError, setIsError] = useState(undefined);
+
+  useEffect(() => {
+    fetchDataGet(mainConst.getSearchId, (data) => {
+      setSearchId(data["searchId"]);
+    }, (error) => {
+      setIsError(error);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (searchId) {
+      fetchDataGet(mainConst.getTickets + `?searchId=${searchId}`, (data) => {
+        console.log(data);
+        if (data) {
+          setTickets(state => state.concat(data["tickets"]));
+          if (!data["stop"]) {
+            setIsTry(state => state + 1);
+          }
+          setIsLoading(data["stop"]);
+        }
+      }, (error) => {
+        console.error(error);
+        setIsTry(state => state + 1);
+      });
+    }
+  }, [searchId, isTry]);
 
   return (
-    <div className="page">
-      <header className="header">
-        <img src={logo} className="main-logo" alt="logo" />
-      </header>
-      <main className="content">
-        <aside className="content__filter">
-          <Filter currentStops={stops} setNewStops={setStops} />
-        </aside>
-        <article className="content__results">
-          <header className="content__tabs">
-            <Tabs currentTab={tab} setNewTab={setTab} />
-          </header>
-          <section className="content__tickets">
-            {tab} {stops.toString()}
-          </section>
-        </article>
-      </main>
-    </div>
+    <>
+      {isError ? <div>Ошибка {isError}</div>
+        : !isLoading ? (
+          <div>Загрузка</div>
+        ) :
+          (
+            <div className="page">
+              <header className="header">
+                <img src={logo} className="main-logo" alt="logo" />
+              </header>
+              <main className="content">
+                <aside className="content__filter">
+                  <Filter currentStops={stops} setNewStops={setStops} />
+                </aside>
+                <article className="content__results">
+                  <header className="content__tabs">
+                    <Tabs currentTab={tab} setNewTab={setTab} />
+                  </header>
+                  <section className="content__tickets">
+                    {tab} {stops.toString()} {tickets.toString()}
+                  </section>
+                </article>
+              </main>
+            </div>
+          )}
+    </>
   );
 }
 
--- src/constants/main.js
+++ src/constants/main.js
@@ -1,3 +1,4 @@
 export const mainConst = {
     getSearchId: "https://front-test.beta.aviasales.ru/search",
+    getTickets:"https://front-test.beta.aviasales.ru/tickets"
 }
